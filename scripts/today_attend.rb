61b28429367fd34de5bf8dffcdaaf8d199f95a5e
shiotomo <t.shiota16@gmail.com>
2018-02-16 06:00:33 +0000

    today_attend.rbを修正

--- scripts/today_attend.rb
+++ scripts/today_attend.rb
@@ -2,7 +2,7 @@ module Ruboty
   module Handlers
     class Today_attend < Base
       # ここを変更すると返すやつを変えれる
-      on(/今日の出席\z/i, name: "today_attend", description: "Return Hello to World")
+      on(/今日の出席/, name: "today_attend", description: "Return Hello to World")
 
       def today_attend(message)
         message.reply('今日の出席を返します')
