5de91b7f251f289a7b9ce7eb310af37eb0cf44a2
shiotomo <t.shiota16@gmail.com>
2018-02-16 08:03:16 +0000

    demo deploy

--- scripts/attend.rb
+++ scripts/attend.rb
@@ -1,4 +1,6 @@
-# require_relative 'db_config'
+require_relative 'db_config'
+
+require "pg"
 
 module Ruboty
   module Handlers
@@ -7,17 +9,38 @@ module Ruboty
       on(/出席/, name: "attend", description: "Return Hello to World")
 
       def attend(message)
-        # con = DbConfig.new().con
+        con = DbConfig.new().con
 
-        p message
+        # 学籍番号取得
+        p message.from_name
+        number =  message.from_name
+
+        date = Date.today
+
+        date = "attends" + date.to_s.tr("-", "_")
+
+        puts date
 
         # テーブルの作成
-        # begin
-        #   con.exec("create table attends (student_number varchar(80);")
-        # rescue
-        #   puts "テーブルが存在します"
+        begin
+          con.exec("create table #{date} (student_number varchar(80));")
+          puts "yahoo!"
+        rescue
+          puts "テーブルが存在します"
+        end
+
+        # exsit_number = con.exec("select '#{number}' from #{date}")
+
+        # exsit_number = exsit_number.to_s
+
+        # if !exsit_number.include?("#{number}") 
+        #   message.reply('出席してます')
+        # else
+        #   con.exec("insert into #{date} (student_number) values ('#{number}');")
+        #   message.reply('出席します')
         # end
 
+        con.exec("insert into #{date} (student_number) values ('#{number}');")
         message.reply('出席します')
       end
     end
--- scripts/today_attend.rb
+++ scripts/today_attend.rb
@@ -1,3 +1,7 @@
+require_relative 'db_config'
+
+require "pg"
+
 module Ruboty
   module Handlers
     class Today_attend < Base
@@ -5,7 +9,19 @@ module Ruboty
       on(/今日の出席/, name: "today_attend", description: "Return Hello to World")
 
       def today_attend(message)
-        message.reply('今日の出席を返します')
+        con = DbConfig.new().con
+        date = Date.today
+
+        date = "attends" + date.to_s.tr("-", "_")
+
+        mes = con.exec("select * from #{date};")
+
+        mes.each do |d|
+          puts d.keys
+          puts d
+          message.reply(d.values)
+        end
+
       end
     end
   end
