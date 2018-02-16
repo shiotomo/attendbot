594f764258b6dbfd43fc8a98301aeef9d07950df
shiotomo <t.shiota16@gmail.com>
2018-02-16 05:35:22 +0000

    Fix: hello.rb

--- scripts/hello.rb
+++ scripts/hello.rb
@@ -1,7 +1,7 @@
 module Ruboty
   module Handlers
     class HelloWorld < Base
-      on(/hello\z/i, name: "hello_world", description: "Return Hello to World")
+      on(/good\z/i, name: "hello_world", description: "Return Hello to World")
 
       def hello_world(message)
         message.reply('World!')
