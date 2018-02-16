6229955e602280e673f96d4ea4284643796653f3
shiotomo <t.shiota16@gmail.com>
2018-02-16 05:50:49 +0000

    今日の日程とリプを送ると今日の日程です

--- scripts/hello.rb
+++ scripts/hello.rb
@@ -2,10 +2,10 @@ module Ruboty
   module Handlers
     class HelloWorld < Base
       # ここを変更すると返すやつを変えれる
-      on(/good\z/i, name: "hello_world", description: "Return Hello to World")
+      on(/今日の日程\z/i, name: "hello_world", description: "Return Hello to World")
 
       def hello_world(message)
-        message.reply('World!')
+        message.reply('今日の日程を返します!')
       end
     end
   end
