466cfb0d2df29bffb2f4932dba1cc114b4187f36
shiotomo <t.shiota16@gmail.com>
2018-02-16 05:38:03 +0000

    hello.rbコメントを追加した

--- scripts/hello.rb
+++ scripts/hello.rb
@@ -1,6 +1,7 @@
 module Ruboty
   module Handlers
     class HelloWorld < Base
+      # ここを変更すると返すやつを変えれる
       on(/good\z/i, name: "hello_world", description: "Return Hello to World")
 
       def hello_world(message)
